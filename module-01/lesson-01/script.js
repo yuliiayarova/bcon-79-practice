f10bd326fe106e8926214f2f394ed9fd4d57051c
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-01-09 14:22:17 +0100

    add new folder lesson-02

---
+++ module-01/lesson-01/script.js
@@ -0,0 +1,104 @@
+// 1
+const myName = "Yuliia";
+console.log("🚀 ~ myName:", myName);
+
+// 2
+const month = 11;
+console.log("🚀 ~ month:", month);
+
+// 3
+const text = "Ласкаво" + " " + "просимо" + " " + "на" + " " + "курс";
+console.log("🚀 ~ text:", text);
+
+// 4
+// alert(2019);
+
+// 5
+const result = 2019 - 200;
+console.log("🚀 ~ result:", result);
+
+// 6
+const containerEl = document.getElementById("six");
+console.log("🚀 ~ containerEl:", containerEl);
+containerEl.textContent = "Hello world";
+
+// 7
+const wrapperEl = document.getElementById("seven");
+console.log("🚀 ~ wrapperEl:", wrapperEl);
+wrapperEl.textContent = 12 * 12;
+
+// 8
+const out8 = document.querySelector(".out-8");
+console.log("🚀 ~ out8:", out8);
+out8.textContent = "task-8";
+
+// 9
+
+const out9 = document.querySelector(".out-9");
+console.log("🚀 ~ out9:", out9);
+out9.textContent = "World";
+
+// 10
+const out10 = document.querySelector(".out-10");
+console.log("🚀 ~ out10:", out10);
+out10.innerHTML = "<h2>Hi</h2>";
+
+// 11
+const out11 = document.querySelector(".out-11");
+console.log("🚀 ~ out11:", out11);
+out11.innerHTML = "123";
+out11.innerHTML += "456";
+
+// 12
+const a = document.querySelector(".out-12");
+console.log("🚀 ~ a:", a);
+a.innerHTML = 3.1415;
+//* a.innerHTML = Math.PI;
+
+// 13
+const out13 = document.querySelector(".out-13");
+console.log("🚀 ~ out13:", out13);
+out13.innerHTML =
+  '<img src="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a" alt="js" width="400" >';
+
+// 14
+const z1 = 6;
+const z2 = 3;
+const out14 = document.querySelector(".out-14");
+console.log("🚀 ~ out14:", out14);
+out14.innerHTML = z1 * z2;
+
+// 15
+const y1 = 6;
+const y2 = 3;
+const out15 = document.querySelector(".out-15");
+console.log("🚀 ~ out15:", out15);
+out15.innerHTML = y1 / y2;
+
+// 16
+const x1 = "Hello";
+const x2 = 5;
+const out16 = document.querySelector(".out-16");
+console.log("🚀 ~ out16:", out16);
+out16.innerHTML = x1 + x2;
+
+// 17
+const out17 = document.querySelector(".out-17");
+console.log("🚀 ~ out17:", out17);
+
+// 18
+// const out18 = document.querySelector(".out-18");
+// console.log("🚀 ~ out18:", out18);
+//! out18 = 5;
+// console.log("🚀 ~ out18:", out18);
+
+// 19
+// const out19 = document.querySelector(".out-19");
+// console.log("🚀 ~ out19:", out19);
+//! out19 = document.querySelector(".out-19-test");
+// console.log("🚀 ~ out19:", out19);
+
+// 20
+const out20 = document.querySelector(".out-20");
+console.log("🚀 ~ out20:", out20);
+out20.textContent = "<h2>Hi</h2>";
