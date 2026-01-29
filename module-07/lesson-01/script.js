ff96c288d53500db2280698d06ddba0314755cbd
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-01-29 14:49:34 +0100

    add folder module-7

---
+++ module-07/lesson-01/script.js
@@ -0,0 +1,39 @@
+// Task 01
+
+// Task 02
+
+// Task 03
+
+// Task 04
+
+// Task 05
+
+// Task 06
+
+// Task 07
+
+// Task 08
+
+// Task 09
+
+// Task 10
+
+// Task 11
+
+// Task 12
+
+// Task 13
+
+// Task 14
+
+// Task 15
+
+// Task 16
+
+// Task 17
+
+// Task 18
+
+// Task 19
+
+// Task 20
